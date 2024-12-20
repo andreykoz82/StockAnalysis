7b5feda2a34a29c9b3ea68d0e654adae6d45bf53
andreykozinov <kozinov.andrey@gmail.com>
2024-12-20 21:41:58 +0300

    Update

--- sql/remaining_stock.sql
+++ sql/remaining_stock.sql
@@ -10,12 +10,20 @@ WITH combined_data AS (
         sales_forecast sf
     ON
         cs.Номенклатура = sf.item
+    WHERE
+        sf.ds >= CURRENT_DATE -- Условие для выбора данных только с текущей даты
 ),
 running_stock AS (
     SELECT
         Номенклатура,
         ds,
-        GREATEST(initial_stock - SUM(timesfm) OVER (PARTITION BY Номенклатура ORDER BY ds), 0) AS remaining_stock
+        GREATEST(
+            initial_stock - SUM(timesfm) OVER (
+                PARTITION BY Номенклатура
+                ORDER BY ds
+            ),
+            0
+        ) AS remaining_stock
     FROM
         combined_data
 )
