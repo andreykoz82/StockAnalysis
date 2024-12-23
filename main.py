d4925d327b05a9fc622bb00a283ae26cf55f4104
Andrey <kozinov.andrey@gmail.com>
2024-12-23 15:06:36 +0300

    Update

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import warnings
 import pandas as pd
 from sqlalchemy import create_engine
 import timesfm
-import numpy as np
-
+import os
 warnings.filterwarnings('ignore')
+os.environ['CURL_CA_BUNDLE'] = ''
 
 # Подключение к базе данных
 engine = create_engine('postgresql+psycopg2://gen_user:Body0906rock@93.183.81.166/stock_analysis')
@@ -23,10 +23,21 @@ item_list = actual_items['Наименование'].sort_values().to_list()
 train_end = '2024-11-30'
 forecast_start = '2024-12-01'
 forecast_end = '2025-01-31'
+prediction_length = 2
 
 forecast_results = []
 
 sales_forecast = pd.DataFrame([{'ds':None, 'timesfm':None, 'item':None}])
+
+tfm = timesfm.TimesFm(
+        hparams=timesfm.TimesFmHparams(
+            backend="cpu",
+            per_core_batch_size=32,
+            horizon_len=prediction_length,
+        ),
+        checkpoint=timesfm.TimesFmCheckpoint(
+            huggingface_repo_id="google/timesfm-1.0-200m-pytorch"),
+    )
 
 for item in item_list:
     # Выгрузка данных по номенклатуре
@@ -40,22 +51,12 @@ for item in item_list:
     if sales_by_item.empty:
         continue
 
-    train = sales_by_item[sales_by_item.index < train_end].resample('M').sum()
-    prediction_length = (pd.Timestamp(forecast_end) - pd.Timestamp(forecast_start)).days
+    train = sales_by_item[sales_by_item.index <= train_end].resample('M').sum()
 
-    # Модель Google TimesFM
     df = pd.DataFrame({'unique_id': [1] * len(train),
                        'ds': train.index,
                        "y": train['Продажи']})
-    tfm = timesfm.TimesFm(
-        hparams=timesfm.TimesFmHparams(
-            backend="cpu",
-            per_core_batch_size=32,
-            horizon_len=prediction_length,
-        ),
-        checkpoint=timesfm.TimesFmCheckpoint(
-            huggingface_repo_id="google/timesfm-1.0-200m-pytorch"),
-    )
+
     timesfm_forecast = tfm.forecast_on_df(
         inputs=df,
         freq="M",
