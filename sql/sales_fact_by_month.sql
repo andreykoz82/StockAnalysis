b2fa4bdbdd044442091ced46a7fd6ad68e8aad54
andreykozinov <kozinov.andrey@gmail.com>
2024-12-23 21:55:55 +0300

    Update

---
+++ sql/sales_fact_by_month.sql
@@ -0,0 +1,11 @@
+SELECT
+    DATE_TRUNC('month', Дата) + INTERVAL '1 month - 1 day' AS Дата,
+    Номенклатура,
+    SUM(Продажи) AS ОбщиеПродажи
+FROM
+    sales
+GROUP BY
+    DATE_TRUNC('month', Дата) + INTERVAL '1 month - 1 day',
+    Номенклатура
+ORDER BY
+    Дата, Номенклатура;
