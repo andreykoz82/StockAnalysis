6b6654d7febe144d41e39790d77d5bbc28444aaf
Andrey <kozinov.andrey@gmail.com>
2025-01-17 08:33:54 +0300

    Update

--- main.py
+++ main.py
@@ -10,28 +10,25 @@ os.environ['CURL_CA_BUNDLE'] = ''
 # Подключение к базе данных
 engine = create_engine('postgresql+psycopg2://gen_user:Body0906rock@93.183.81.166/stock_analysis')
 
-# %%
 # Обновление текущих остатков НФП
 current_stocks = pd.read_excel('data/semifinished_stocks.xlsx')
 current_stocks.to_sql('semifinished_stocks', con=engine, if_exists='replace', index=False)
 
-# %%
 # Обновление текущих остатков готовой продукции
 current_stocks = pd.read_excel('data/finished_goods_stocks.xlsx')
 current_stocks['По дням'] = pd.to_datetime(current_stocks['По дням'], dayfirst=True)
 current_stocks.to_sql('current_stocks', con=engine, if_exists='replace', index=False)
 
-# %%
 # Обновление текущих продаж
 sales = pd.read_excel('data/sales.xlsx')
 sales['Дата'] = pd.to_datetime(sales['Дата'])
 sales.to_sql('sales', con=engine, if_exists='replace', index=False)
 
-# %%
 # Текущие заказы
 actual_orders = pd.read_excel('data/actual_orders_2stage.xlsx')
 actual_orders.to_sql('actual_orders', con=engine, if_exists='replace', index=False)
-# %%
+
+
 # Обновление остатков печатных материалов
 materials_current_stocks = pd.read_excel('data/materials_stocks.xlsx')
 materials_current_stocks.to_sql('materials_stocks', con=engine, if_exists='replace', index=False)
